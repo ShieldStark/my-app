e960091de362110d1d98785e8af32e3bb79eaa4c
Piyush <piyushabhiranjan30@gmail.com>
2022-12-17 16:10:09 +0530

    Multiple State

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import ExpenseItem from './components/Expenses/ExpenseItem.js'
 import './App.css';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
   const expenses = [
@@ -28,7 +29,7 @@ function App() {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense/>
       <Expenses items={expenses}/>
     </div>
   );
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,11 @@
-import './NewExpense.css'
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
 
-const NewExpense = ()=>{
-    return <div className='new-expense'>
-        <form></form>
+const NewExpense = () => {
+  return (
+    <div className="new-expense">
+      <ExpenseForm />
     </div>
-}
+  );
+};
+export default NewExpense;
